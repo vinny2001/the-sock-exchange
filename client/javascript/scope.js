a478f9b0f29fbc8595d6255f7c87d5cea09276e8
wasadmin <wasadmin@example.com>
2024-06-18 13:23:47 -0400

    Lab-4.3

---
+++ client/javascript/scope.js
@@ -0,0 +1,24 @@
+// 1. Global scope
+let globalVar = "I am global";
+
+function showMessage() {
+  console.log(globalVar); // Accessible here
+}
+
+showMessage(); // Outputs: I am global
+console.log(globalVar); // Also accessible here
+
+// 2. Local scope
+function greet() {
+    let greeting = "Hello World";
+    console.log(greeting); // Accessible here
+}
+let greeting = "Hello There!"
+greet(); // Outputs: Hello World
+console.log(greeting); // Error: greeting is not defined
+
+// 3. Block scope
+if (true) {
+    let blockVar = "I am inside a block";
+    console.log(blockVar); // Accessible here
+}
