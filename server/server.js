21ab23edf36dc52b775cfce29e997e16582c0924
vinny2001 <vincenzo.daria01@gmail.com>
2024-06-28 11:57:33 -0400

    MERN Lab 8

--- server/server.js
+++ server/server.js
@@ -1,34 +1,100 @@
 import express from 'express';
 import { promises as fs } from 'fs';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
 
 const app = express();
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Middleware to parse JSON bodies
 const PORT = 3000;
 
 // Endpoint to read and send JSON file content
 app.get('/socks', async (req, res) => {
     try {
-        // Console log the entire request object
-        console.log(req);
-
-        // Console log specific parts of the request
-        console.log("Headers:", req.headers);
-        console.log("URL:", req.url);
-        console.log("Method:", req.method);
-        console.log("Query parameters:", req.query);
-
-        const data = await fs.readFile('../data/socks.json', 'utf8');
-        const jsonObj = JSON.parse(data);
-        res.json(jsonObj);
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const socks = await collection.find({}).toArray();
+        res.json(socks);
     } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
     }
 });
 
-// Middleware to parse JSON bodies
-app.use(express.json());
+app.post('/socks/search', async (req, res) => {
+    try {
+        const { searchTerm } = req.body;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const regex = new RegExp(searchTerm, 'i'); // Create a case-insensitive regular expression
+        const socks = await collection.find({ 'sockDetails.color': regex }).toArray();
+        res.json(socks);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error searching for socks');
+    }
+});
+
+app.delete('/socks/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const result = await collection.deleteOne({ _id: new ObjectId(id) });
+        if (result.deletedCount === 1) {
+            res.status(200).send('Sock deleted successfully');
+        } else {
+            res.status(404).send('Sock not found');
+        }
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error deleting sock');
+    }
+});
 
 app.post('/socks', async (req, res) => {
+    try {
+        const sock  = req.body;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const result = await collection.insertOne(sock);
+        res.status(201).send(`{"_id":"${result.insertedId}"}`);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error adding sock');
+    }
+});
+
+app.get('/socks/:color', async (req, res) => {
+    try {
+        const { color } = req.params;
+
+        const data = await fs.readFile('../data/socks.json', 'utf8');
+        const jsonObj = JSON.parse(data);
+        const result = jsonObj.filter(sock => sock.color.toUpperCase() === color.toUpperCase());
+        if(result.length === 0) {
+            return res.status(404).send("No socks found with that color.");
+        }
+        res.json(result);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+
+
+app.post('/user', async (req, res) => {
     try {
         // Obligatory reference to POST Malone
         console.log("If POST Malone were a sock, he'd be the one with the most colorful pattern.");
@@ -49,10 +115,6 @@ app.post('/socks', async (req, res) => {
         console.error("Error:", err);
         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
     }
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 app.delete('/socks/:id', async (req, res) => {
@@ -82,19 +144,6 @@ app.put('/user/:id', async (req, res) => {
     }
 });
 
-app.get('/socks/:color', async (req, res) => {
-    try {
-        const { color } = req.params;
-
-        const data = await fs.readFile('../data/socks.json', 'utf8');
-        const jsonObj = JSON.parse(data);
-        const result = jsonObj.filter(sock => sock.color.toUpperCase() === color.toUpperCase());
-        if(result.length === 0) {
-            return res.status(404).send("No socks found with that color.");
-        }
-        res.json(result);
-    } catch (err) {
-        console.error("Error:", err);
-        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
-    }
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
